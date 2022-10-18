b17a5f8e969512b5cd160471a2d2101805521ce9
cluck135 <casenluck@gmail.com>
2022-10-18 15:28:47 -0400

    first

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    // Update is called once per frame
+    public float shotInterval = 3;
+    void Start() {
+        InvokeRepeating("Timer", 0, 1);
+    }
+    void Timer(){
+        shotInterval++;
+    }
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(shotInterval > 2) {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                shotInterval = 0;
+            }
+        }
+    }
+}
